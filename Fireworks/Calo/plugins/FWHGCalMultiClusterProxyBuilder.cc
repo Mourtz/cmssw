7e5cda478935bbcdec5e4d39784512b626fa59d7
Alex Mourtziapis <alex.mourtziapis@cern.ch>
2019-01-15 16:55:49 +0100

    Update "FWHGCalMultiClusterProxyBuilder"

--- Fireworks/Calo/plugins/FWHGCalMultiClusterProxyBuilder.cc
+++ Fireworks/Calo/plugins/FWHGCalMultiClusterProxyBuilder.cc
@@ -22,55 +22,47 @@ private:
 };
 
 void
-FWHGCalMultiClusterProxyBuilder::build( const reco::HGCalMultiCluster& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* )
+FWHGCalMultiClusterProxyBuilder::build(const reco::HGCalMultiCluster &iData, unsigned int iIndex, TEveElement &oItemHolder, const FWViewContext *)
 {
-  const auto & clusters = iData.clusters();
+   const auto &clusters = iData.clusters();
 
-  TEveBoxSet* boxset = new TEveBoxSet();
-  boxset->Reset(TEveBoxSet::kBT_FreeBox, true, 64);
-  boxset->UseSingleColor();
-  boxset->SetPickable(true);
-  for (const auto & c : clusters)
-    {
-      std::vector<std::pair<DetId, float> > clusterDetIds = c->hitsAndFractions();
+   bool h_hex(false);
+   TEveBoxSet *hex_boxset = new TEveBoxSet("Silicon");
+   hex_boxset->UseSingleColor();
+   hex_boxset->SetPickable(true);
+   hex_boxset->Reset(TEveBoxSet::kBT_Hex, true, 64);
+   hex_boxset->SetAntiFlick(kTRUE);
 
+   bool h_box(false);
+   TEveBoxSet *boxset = new TEveBoxSet("Scintillator");
+   boxset->UseSingleColor();
+   boxset->SetPickable(true);
+   boxset->Reset(TEveBoxSet::kBT_FreeBox, true, 64);
+   boxset->SetAntiFlick(kTRUE);
 
-      for( std::vector<std::pair<DetId, float> >::iterator it = clusterDetIds.begin(), itEnd = clusterDetIds.end();
-	   it != itEnd; ++it )
-	{
-      const float* corners = item()->getGeom()->getCorners( it->first );
-      const float* parameters = item()->getGeom()->getParameters( it->first );
-      const float* shapes = item()->getGeom()->getShapePars(it->first);
+   for (const auto &c : clusters)
+   {
+      std::vector<std::pair<DetId, float>> clusterDetIds = c->hitsAndFractions();
 
-      if( corners == nullptr || parameters == nullptr || shapes == nullptr ) {
-         continue;
+      for (std::vector<std::pair<DetId, float>>::iterator it = clusterDetIds.begin(), itEnd = clusterDetIds.end();
+           it != itEnd; ++it)
+      {
+         const float *corners = item()->getGeom()->getCorners(it->first);
+         item()->getGeom()->getHGCalRecHits(it->first, hex_boxset, boxset, h_hex, h_box);
       }
+   }
+   
+   if (h_hex)
+   {
+      hex_boxset->RefitPlex();
+      setupAddElement(hex_boxset, &oItemHolder, true);
+   }
 
-#if 0
-      const int total_points = parameters[0];
-      const int total_vertices = 3*total_points;
-#else // using broken boxes(half hexagon) until there's support for hexagons in TEveBoxSet
-      const int total_points = 4;
-      const int total_vertices = 3*total_points;
-
-      const float thickness = shapes[3];
-
-      std::vector<float> pnts(24);
-      for(int i = 0; i < total_points; ++i){
-         pnts[i*3+0] = corners[i*3];
-         pnts[i*3+1] = corners[i*3+1];
-         pnts[i*3+2] = corners[i*3+2];
-
-         pnts[(i*3+0)+total_vertices] = corners[i*3];
-         pnts[(i*3+1)+total_vertices] = corners[i*3+1];
-         pnts[(i*3+2)+total_vertices] = corners[i*3+2]+thickness;
-      }
-      boxset->AddBox( &pnts[0]);
-#endif
-	}
-    }
-   boxset->RefitPlex();
-   setupAddElement(boxset, &oItemHolder);
+   if (h_box)
+   {
+      boxset->RefitPlex();
+      setupAddElement(boxset, &oItemHolder, true);
+   }
 }
 
 REGISTER_FWPROXYBUILDER( FWHGCalMultiClusterProxyBuilder, reco::HGCalMultiCluster, "HGCal MultiCluster", FWViewType::kISpyBit );
