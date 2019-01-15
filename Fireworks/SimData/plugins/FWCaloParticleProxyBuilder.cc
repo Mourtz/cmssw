e425352eb2d00b8eb8030ddbc58056f87f171dd4
Alex Mourtziapis <alex.mourtziapis@cern.ch>
2019-01-15 17:00:17 +0100

    Update "FWCaloParticleProxyBuilder"

--- Fireworks/SimData/plugins/FWCaloParticleProxyBuilder.cc
+++ Fireworks/SimData/plugins/FWCaloParticleProxyBuilder.cc
@@ -21,84 +21,63 @@
 #include "DataFormats/DetId/interface/DetId.h"
 
 #include "Fireworks/Core/interface/FWProxyBuilderConfiguration.h"
-#include "Fireworks/Core/interface/FWParameters.h"
 
-#include "TGeoBBox.h"
-#include "TGeoXtru.h"
-
-#include "TEveGeoShape.h"
-#include "TEveTrack.h"
 #include "TEveBoxSet.h"
+#include "TEveCompound.h"
 
-class FWCaloParticleProxyBuilder : public FWProxyBuilderBase
+class FWCaloParticleProxyBuilder : public FWSimpleProxyBuilderTemplate<CaloParticle>
 {
-public:
-   FWCaloParticleProxyBuilder( void ) {}
-   ~FWCaloParticleProxyBuilder( void ) override {}
+ public:
+   FWCaloParticleProxyBuilder(void) {}
+   ~FWCaloParticleProxyBuilder(void) override {}
 
    REGISTER_PROXYBUILDER_METHODS();
-private:
-   // Disable default copy constructor
-   FWCaloParticleProxyBuilder( const FWCaloParticleProxyBuilder& ) = delete;
-   // Disable default assignment operator
-   const FWCaloParticleProxyBuilder& operator=( const FWCaloParticleProxyBuilder& ) = delete;
 
-   void build( const FWEventItem* iItem, TEveElementList* product, const FWViewContext* ) override;
+ private:
+   // Disable default copy constructor
+   FWCaloParticleProxyBuilder(const FWCaloParticleProxyBuilder &) = delete;
+   // Disable default assignment operator
+   const FWCaloParticleProxyBuilder &operator=(const FWCaloParticleProxyBuilder &) = delete;
+
+   // using FWSimpleProxyBuilderTemplate<CaloParticle>::build;
+   void build(const CaloParticle &iData, unsigned int iIndex, TEveElement &oItemHolder, const FWViewContext *) override;
 };
 
-void
-FWCaloParticleProxyBuilder::build( const FWEventItem* iItem, TEveElementList* product, const FWViewContext* ){
-   const CaloParticleCollection* collection = nullptr;
-   iItem->get( collection );
+void FWCaloParticleProxyBuilder::build(const CaloParticle &iData, unsigned int iIndex, TEveElement &oItemHolder, const FWViewContext *)
+{
+   bool h_hex(false);
+   TEveBoxSet *hex_boxset = new TEveBoxSet("Silicon");
+   hex_boxset->UseSingleColor();
+   hex_boxset->SetPickable(true);
+   hex_boxset->Reset(TEveBoxSet::kBT_Hex, true, 64);
+   hex_boxset->SetAntiFlick(kTRUE);
 
-   if( collection == nullptr ) return;
+   bool h_box(false);
+   TEveBoxSet *boxset = new TEveBoxSet("Scintillator");
+   boxset->UseSingleColor();
+   boxset->SetPickable(true);
+   boxset->Reset(TEveBoxSet::kBT_FreeBox, true, 64);
+   boxset->SetAntiFlick(kTRUE);
 
-   for( const auto & iData : *collection )
+   for (const auto &c : iData.simClusters())
    {
-      TEveBoxSet* boxset = new TEveBoxSet();
-      boxset->Reset(TEveBoxSet::kBT_FreeBox, true, 64);
-      boxset->UseSingleColor();
-      boxset->SetPickable(true);
-
-      for (const auto & c : iData.simClusters())
+      for (const auto &it : (*c).hits_and_fractions())
       {
-         for( const auto & it : (*c).hits_and_fractions() )
-         {  
-            const float* corners = item()->getGeom()->getCorners( it.first );
-            const float* parameters = item()->getGeom()->getParameters( it.first );
-            const float* shapes = item()->getGeom()->getShapePars(it.first);
-
-            if( corners == nullptr || parameters == nullptr || shapes == nullptr ) {
-               continue;
-            }
-
-#if 0
-            const int total_points = parameters[0];
-            const int total_vertices = 3*total_points;
-#else // using broken boxes(half hexagon) until there's support for hexagons in TEveBoxSet
-            const int total_points = 4;
-            const int total_vertices = 3*total_points;
-
-            const float thickness = shapes[3];
-
-            std::vector<float> pnts(24);
-            for(int i = 0; i < total_points; ++i){
-               pnts[i*3+0] = corners[i*3];
-               pnts[i*3+1] = corners[i*3+1];
-               pnts[i*3+2] = corners[i*3+2];
-
-               pnts[(i*3+0)+total_vertices] = corners[i*3];
-               pnts[(i*3+1)+total_vertices] = corners[i*3+1];
-               pnts[(i*3+2)+total_vertices] = corners[i*3+2]+thickness;
-            }
-            boxset->AddBox( &pnts[0]);
-#endif
-
-         }
+         item()->getGeom()->getHGCalRecHits(it.first, hex_boxset, boxset, h_hex, h_box);
       }
+   }
+
+   if (h_hex)
+   {
+      hex_boxset->RefitPlex();
+      setupAddElement(hex_boxset, &oItemHolder, true);
+   }
+
+   if (h_box)
+   {
       boxset->RefitPlex();
-      setupAddElement(boxset, product);
+      setupAddElement(boxset, &oItemHolder, true);
    }
 }
 
-REGISTER_FWPROXYBUILDER( FWCaloParticleProxyBuilder, CaloParticleCollection, "CaloFTW", FWViewType::kAll3DBits | FWViewType::kAllRPZBits );
+REGISTER_FWPROXYBUILDER(FWCaloParticleProxyBuilder, CaloParticle, "CaloParticle", FWViewType::kAll3DBits | FWViewType::kAllRPZBits);
